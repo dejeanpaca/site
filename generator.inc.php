161190de544fc94d46e5d036991113e3d097c01f
Dejan Boras <de.jean.7777@protonmail.com>
2019-06-06 00:09:17 +0200

    Improve site and make an initial post

--- generator.inc.php
+++ generator.inc.php
@@ -5,10 +5,6 @@ Common::$copy_list = ['css', 'js'];
 Common::Add('__HEADER__', 'header.html');
 Common::Add('__FOOTER__', 'footer.html');
 
-Pages::add_post('Start', 'start.html', '2019-05-28');
-Pages::add_post('Another one', 'start.html', '2019-05-29');
-Pages::add_post('Where to', 'start.html', '2019-05-30');
-
-Pages::add_page('About', 'about.html');
+Pages::add_post('Projects', 'projects.html', '2019-06-06');
 
 Pages::add_index('Home', 'index.html');
