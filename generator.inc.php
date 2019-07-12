bc1844f2f85755157b0f1b921c1a30ea9cd4eb6f
Dejan Boras <de.jean.7777@protonmail.com>
2019-07-12 15:58:46 +0200

    Add about page and images

--- generator.inc.php
+++ generator.inc.php
@@ -1,10 +1,11 @@
 <?php
 
-Common::$copy_list = ['css', 'js'];
+Common::$copy_list = ['css', 'js', 'images'];
 
 Common::Add('__HEADER__', 'header.html');
 Common::Add('__FOOTER__', 'footer.html');
 
 Pages::add_post('Projects', 'projects.html', '2019-06-06');
 
+Pages::add_page('About', 'about.html');
 Pages::add_index('Home', 'index.html');
