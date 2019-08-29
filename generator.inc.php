efac66ab2852e842a7c327e7e0c3a99d0d47f9a6
Dejan Boras <de.jean.7777@protonmail.com>
2019-08-29 17:26:15 +0200

    Update site to changes in the generator

--- generator.inc.php
+++ generator.inc.php
@@ -1,11 +1,9 @@
 <?php
 
+require_once include_module('css');
+require_once include_module('rss');
+
 Common::$copy_list = ['css', 'images'];
 
 Common::Add('__HEADER__', 'header.html');
 Common::Add('__FOOTER__', 'footer.html');
-
-Pages::add_post('Projects', 'projects.html', '2019-06-06');
-
-Pages::add_page('About', 'about.html');
-Pages::add_index('Home', 'index.html');
