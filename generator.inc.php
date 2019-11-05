1c4eb5419097860301ae49f07edd7c120ac0c424
Dejan Boras <de.jean.7777@protonmail.com>
2019-11-05 23:07:53 +0100

    Some preparation for a grumpy corner to the site

--- generator.inc.php
+++ generator.inc.php
@@ -1,12 +1,30 @@
 <?php
 
-require_once include_module('css');
 require_once include_module('atom');
 
 Common::$copy_list = ['css', 'images', 'js'];
 
 Common::Add('__FOOTER__', 'footer.html');
 Common::Add('__POST_HEADER__', 'post_header.html');
+
+Common::CreateCategory('grumpy', '__GRUMPY_POSTS_LIST__');
+
+$grumpyPageType = new PageType();
+$grumpyPageType->templateFile = 'post_template.html';
+$grumpyPageType->SetDirectory('grumpy');
+$grumpyPageType->category = 'grumpy';
+$grumpyPageType->class = '\GrumpyPost';
+
+class GrumpyPost extends Page
+{
+    function __construct() {
+        global $grumpyPageType;
+
+        parent::__construct();
+
+        $this->setType($grumpyPageType);
+    }
+}
 
 Common::$markers->Add('__AUTHOR_NAME__', 'Dejan Boras');
 Common::$markers->Add('__SITE_LINK__', 'https://dbx7.net');
